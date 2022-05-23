a00b20713e34a4410c90dd744fc57a9081b14672
Александр <a@kistruga.ru>
2022-05-23 14:21:08 +0400

    ДЗ №4 Циклы Задание №1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    public static void task1(){
+        int i = 0;
+        while (i < 10){
+            i++;
+            System.out.print(i + " " );
+       }
+        System.out.println();
+        for ( i = 10; i > 0; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
