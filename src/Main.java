0fe43267797b3392a639b1406932ac6768a2ff80
Александр <a@kistruga.ru>
2022-05-23 14:37:09 +0400

    ДЗ №4 Циклы Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
-    public static void task1(){
+
+    public static void task1() {
         int i = 0;
-        while (i < 10){
+        while (i < 10) {
             i++;
-            System.out.print(i + " " );
-       }
-        System.out.println();
-        for ( i = 10; i > 0; i--){
             System.out.print(i + " ");
+        }
+        System.out.println();
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void task2() {
+        int i = 6;
+        int daysMonth = 31;
+        for (; i <= daysMonth; i = i + 7) {
+            System.out.println("Сегоня пятница " + i + " -е. Необходимо подготовить отчет");
         }
     }
 }
