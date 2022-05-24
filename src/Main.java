4dfb187ead25056f2037e6f93a4332c409d237db
Александр <a@kistruga.ru>
2022-05-24 14:46:47 +0400

    ДЗ №4 Циклы Задание №3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
 
@@ -23,5 +24,21 @@ public class Main {
         for (; i <= daysMonth; i = i + 7) {
             System.out.println("Сегоня пятница " + i + " -е. Необходимо подготовить отчет");
         }
+    }
+
+    public static void task3() {
+        int cometFlyby = 79;
+        int currentYear = 2022;
+        int initialYear = currentYear - 200;
+        int finalFear = currentYear + 100;
+        for (int year = initialYear; year < finalFear; year++) {
+            if (year % cometFlyby == 0) {
+                System.out.println(year);
+            }
+        }
+    }
+
+    public static void task4() {
+
     }
 }
