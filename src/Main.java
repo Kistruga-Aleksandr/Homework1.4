b57ab7bd937c46e8bfb1f1c4745920218f84a0ff
Александр <a@kistruga.ru>
2022-05-25 12:54:38 +0400

    ДЗ №4 Циклы Задание №4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
 
     }
 
@@ -28,9 +29,8 @@ public class Main {
 
     public static void task3() {
         int cometFlyby = 79;
-        int currentYear = 2022;
-        int initialYear = currentYear - 200;
-        int finalFear = currentYear + 100;
+        int initialYear = 2022 - 200;
+        int finalFear = 2022 + 100;
         for (int year = initialYear; year < finalFear; year++) {
             if (year % cometFlyby == 0) {
                 System.out.println(year);
@@ -39,6 +39,20 @@ public class Main {
     }
 
     public static void task4() {
+        int number1 = 3;
+        int number2 = 5;
+        for (int i = 1; i <= 30; i++) {
+            if (i % number1 == 0 && i % number2 == 0) {
+                System.out.println(i + ": ping pong");
+            } else if (i % number1 == 0) {
+                System.out.println(i + ": ping");
+            } else if (i % number2 == 0) {
+                System.out.println(i + ": pong");
+            } else {
+                System.out.println(i + ":");
+            }
+
+        }
 
     }
 }
